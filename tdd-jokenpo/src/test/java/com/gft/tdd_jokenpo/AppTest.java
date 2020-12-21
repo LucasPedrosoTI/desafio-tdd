597127ec25ad6cf1e1b9c7ee2f3b893ff146800b
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-12-21 15:22:06 -0300

    red - test pedra ganha de tesoura

--- tdd-jokenpo/src/test/java/com/gft/tdd_jokenpo/AppTest.java
+++ tdd-jokenpo/src/test/java/com/gft/tdd_jokenpo/AppTest.java
@@ -1,6 +1,8 @@
 package com.gft.tdd_jokenpo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,10 @@ import com.gft.tdd_jokenpo.model.Opcao;
 
 class AppTest {
 
+	private static final Opcao PEDRA = Opcao.PEDRA;
+	private static final Opcao TESOURA = Opcao.TESOURA;
+	private static final Opcao PAPEL = Opcao.PAPEL;
+
 	Jogador jogador1;
 
 	Jogador jogador2;
@@ -18,16 +24,27 @@ class AppTest {
 	@BeforeEach
 	public void setup() throws Exception {
 		jogador1 = Jogador.builder().build();
-
 		jogador2 = Jogador.builder().build();
+	}
+
+	private void setarOpcoes(Opcao opcao1, Opcao opcao2) {
+		jogador1.setOpcao(opcao1);
+		jogador2.setOpcao(opcao2);
 	}
 
 	@Test
 	void deveEmpatarCasoOpcaoIgual() throws Exception {
-		jogador1.setOpcao(Opcao.PEDRA);
-		jogador2.setOpcao(Opcao.PEDRA);
+		setarOpcoes(PEDRA, PEDRA);
 
 		assertEquals("EMPATE", Juiz.verificar(jogador1, jogador2));
+	}
+
+	@Test
+	void pedraDeveGanharDeTesoura() throws Exception {
+		setarOpcoes(PEDRA, TESOURA);
+
+		assertTrue(jogador1.isVencedor());
+		assertFalse(jogador2.isVencedor());
 	}
 
 }
